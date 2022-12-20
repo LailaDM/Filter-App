d9a6a4160648aacebaa3c1c26d5c7aa8be957866
lail0894@edu.zealand.dk <lail0894@edu.easj.dk>
2022-12-20 23:12:48 +0100

    Filter app

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Home());
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    List<String> countries = [
+      "Polen",
+      "Danmark",
+      "Irak",
+      "Iran",
+      "Sverige",
+      "Ungarn",
+      "Uzbekistan",
+      "Uruguay"
+    ];
+
+    List<String> filter = [];
+    filter.addAll(countries);
+
+    filter.retainWhere((countryone) {
+      return countryone.toLowerCase().contains(
+          "U".toLowerCase()); //you can add another filter conditions too
+    });
+
+    print(filter); //list of countries which contains 'U' on name //output:  [USA, UK, Uganda, Uruguay]
+
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("Filtrering i Flutter"),
+            backgroundColor: Colors.purple),
+        body: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.topCenter,
+          child: Column(
+            children: filter.map((countryone) {
+              return Card(child: ListTile(title: Text(countryone)));
+            }).toList(),
+          ),
+        ));
+  }
+}
